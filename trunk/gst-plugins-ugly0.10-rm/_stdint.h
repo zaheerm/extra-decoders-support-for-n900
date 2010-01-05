5c661c100237ce43ea84f13039a0030405f39f11
dominikowski <dominikowski@04a65e14-f986-11de-bfc1-19433896a63b>
2010-01-05 00:04:15 +0000

    Fix2

--- trunk/gst-plugins-ugly0.10-rm/_stdint.h
+++
@@ -1,9 +0,0 @@
-#ifndef _GST_PLUGINS_UGLY__STDINT_H
-#define _GST_PLUGINS_UGLY__STDINT_H 1
-#ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "gst-plugins-ugly 0.10.10.2"
-/* generated using gnu compiler sbox-arm-none-linux-gnueabi-cc (GCC) 4.2.1 */
-#define _STDINT_HAVE_STDINT_H 1
-#include <stdint.h>
-#endif
-#endif
