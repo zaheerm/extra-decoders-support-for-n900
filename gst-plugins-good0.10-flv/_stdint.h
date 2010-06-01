3dcd376e84fca8f1de0a4974f446dff3dcbba6b1
zaheermerali <zaheermerali@04a65e14-f986-11de-bfc1-19433896a63b>
2010-06-01 12:56:11 +0000

    Initial import of gst-plugins-good0.10-flv

---
+++ gst-plugins-good0.10-flv/_stdint.h
@@ -0,0 +1,9 @@
+#ifndef _GST_PLUGINS_GOOD__STDINT_H
+#define _GST_PLUGINS_GOOD__STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "gst-plugins-good 0.10.23"
+/* generated using gnu compiler sbox-i486-pc-linux-gnu-cc (GCC) 4.2.1 */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
