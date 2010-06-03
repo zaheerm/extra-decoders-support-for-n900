7d737ed2253489e32a351ddb2cc50721d4c40fb9
zaheermerali <zaheermerali@04a65e14-f986-11de-bfc1-19433896a63b>
2010-06-03 17:52:52 +0000

    Added mkv

---
+++ trunk/gst-plugins-good0.10-mkv/_stdint.h
@@ -0,0 +1,9 @@
+#ifndef _GST_PLUGINS_GOOD__STDINT_H
+#define _GST_PLUGINS_GOOD__STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "gst-plugins-good 0.10.16"
+/* generated using gnu compiler sbox-arm-none-linux-gnueabi-cc (GCC) 4.2.1 */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
