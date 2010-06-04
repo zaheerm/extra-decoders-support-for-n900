00b5862cf8083bc2b300139b73bc322fefe2e996
zaheermerali <zaheermerali@04a65e14-f986-11de-bfc1-19433896a63b>
2010-06-04 10:21:09 +0000

    import of gst-ffmpeg

---
+++ gst-ffmpeg-0.10.9/gst-libs/ext/ffmpeg/libpostproc/postprocess.d
@@ -0,0 +1,7 @@
+libpostproc/postprocess.o: libpostproc/postprocess.c config.h libavutil/avutil.h \
+  libavutil/common.h libavutil/mem.h config.h libavutil/internal.h \
+  libavutil/timer.h libavutil/mathematics.h libavutil/rational.h \
+  libavutil/intfloat_readwrite.h libavutil/log.h libavutil/avutil.h \
+  libavutil/pixfmt.h libpostproc/postprocess.h \
+  libpostproc/postprocess_internal.h libpostproc/postprocess_template.c \
+  libavutil/x86_cpu.h
---
+++ gst-ffmpeg-0.10.9/gst-libs/ext/ffmpeg/libswscale/swscale_avoption.d
@@ -0,0 +1,7 @@
+libswscale/swscale_avoption.o: libswscale/swscale_avoption.c libavutil/avutil.h \
+  libavutil/common.h libavutil/mem.h config.h libavutil/internal.h \
+  libavutil/timer.h libavutil/mathematics.h libavutil/rational.h \
+  libavutil/intfloat_readwrite.h libavutil/log.h libavutil/avutil.h \
+  libavutil/pixfmt.h libavcodec/opt.h libavutil/rational.h \
+  libavcodec/avcodec.h libavutil/avutil.h libswscale/swscale.h \
+  libswscale/swscale_internal.h config.h
